54792674bc36f00cd45fb156ede0e642d689184c
Adnan <adnanjaved9598@gmail.com>
2019-04-22 11:57:21 +0500

    impliment new checks on Register Screen

--- src/com/journaldev/servlet/RegisterServlet.java
+++ src/com/journaldev/servlet/RegisterServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -26,21 +27,25 @@ public class RegisterServlet extends HttpServlet {
 		String password = request.getParameter("password");
 		String name = request.getParameter("name");
 		String country = request.getParameter("country");
+		
 		String errorMsg = null;
+		 if(name == null || name.equals("")){
+				errorMsg = "Name can't be null or empty.";
+			}else
+		
 		if(email == null || email.equals("")){
 			errorMsg = "Email ID can't be null or empty.";
 		}
-		if(password == null || password.equals("")){
+		else if(password == null || password.equals("")){
 			errorMsg = "Password can't be null or empty.";
 		}
-		if(name == null || name.equals("")){
-			errorMsg = "Name can't be null or empty.";
-		}
-		if(country == null || country.equals("")){
+		
+		else if(country == null || country.equals("")){
 			errorMsg = "Country can't be null or empty.";
 		}
 		
 		if(errorMsg != null){
+			request.getSession().invalidate();
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.html");
 			PrintWriter out= response.getWriter();
 			out.println("<font color=red>"+errorMsg+"</font>");
@@ -49,29 +54,52 @@ public class RegisterServlet extends HttpServlet {
 		
 		Connection con = (Connection) getServletContext().getAttribute("DBConnection");
 		PreparedStatement ps = null;
+		ResultSet rs = null;
+		PreparedStatement psTwo = null;
 		try {
+			
+			
+			psTwo = con.prepareStatement("select email from Users where email=?  limit 1");
+			psTwo.setString(1, email);
+			rs = psTwo.executeQuery();
+			if(rs != null && rs.next()){
+	
+				
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/register.html");
+				PrintWriter out= response.getWriter();
+				logger.error("User already found with email="+email);
+				out.println("<font color=red>User already found with email.</font>");
+				rd.include(request, response);
+				
+			
+			}else {
+			
+		
 			ps = con.prepareStatement("insert into Users(name,email,country, password) values (?,?,?,?)");
 			ps.setString(1, name);
 			ps.setString(2, email);
 			ps.setString(3, country);
 			ps.setString(4, password);
-	
 			ps.execute();
-			
 			logger.info("User registered with email="+email);
-			
 			//forward to login page to login
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
 			PrintWriter out= response.getWriter();
 			out.println("<font color=green>Registration successful, please login below.</font>");
 			rd.include(request, response);
+			
+			
+			}
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			logger.error("Database connection problem");
 			throw new ServletException("DB Connection problem.");
 		}finally{
 			try {
-				ps.close();
+				if(psTwo != null)psTwo.close();
+				if(rs != null)rs.close();
+				if(ps != null)ps.close();
 			} catch (SQLException e) {
 				logger.error("SQLException in closing PreparedStatement");
 			}
